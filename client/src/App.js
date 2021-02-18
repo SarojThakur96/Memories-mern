7de7730ad90cef306170e6ba26dd19f49b3835e8
Saroj Thakur <sarojthakur247@gmail.com>
2021-02-18 13:02:08 +0530

    first1 commit

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React,{useEffect,useState} from 'react'
+
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import memories from './images/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+import { useDispatch } from 'react-redux';
+
+import useStyles from './styles.js';
+import { getPosts } from './actions/posts';
+
+export const App = () => {
+     
+      const classes = useStyles();
+      const [currentId,setCurrentId] = useState(0);
+      const dispatch = useDispatch();
+
+      useEffect(() => {
+          dispatch(getPosts())
+      }, [currentId,dispatch])
+
+    return (
+        <div>
+            <Container maxWidth="lg">
+                <AppBar className={classes.appBar} position="static" color='inherit'>
+                    <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                    <img className={classes.image} src={memories} alt='icon' height="60" />
+                </AppBar>
+
+                <Grow in>
+                    <Container>
+                        <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+                            <Grid item xs={12} sm={7}>
+                                {/* <Posts setCurrentId={setCurrentId} /> */}
+                                <Posts setCurrentId={setCurrentId}/>
+                            </Grid>
+                            <Grid item xs={12} sm={4}>
+                                {/* <Form currentId={currentId} setCurrentId={setCurrentId} /> */}
+                                <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Grow>
+            </Container>
+        </div>
+    )
+}
